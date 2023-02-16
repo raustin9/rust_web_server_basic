b4d571d27c9c4418cdfdc1afa4d7004cb1374029
Reagan Austin <raustin9@vols.utk.edu>
2023-02-16 11:18:11 -0500

    handle js and css files

--- src/main.rs
+++ src/main.rs
@@ -22,15 +22,36 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
   let buf_reader = BufReader::new(&mut stream);
-  let request_line = buf_reader.lines().next().unwrap().unwrap();
+  let request_line = String::from(buf_reader.lines().next().unwrap().unwrap());
+  
+  let req: Vec<&str> = request_line.split(' ').collect();
 
-  let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-    ("HTTP/1.1 200 OK", "hello.html")
+  // let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+  //   ("HTTP/1.1 200 OK", "hello.html")
+  // } else {
+  //   println!("reqeust: {}", request_line);
+  //   ("HTTP/1.1 404 NOT FOUND", "404.html")
+  // };
+
+  let (status_line , filename) = if request_line == "GET / HTTP/1.1" {
+    ("HTTP/1.1 200 OK" , "hello.html")
+  } else if req[0] == "GET" && req[1].chars().nth(0).unwrap() == '/' {
+    let mut file_request = req[1];
+    let mut file_request_name: &str = file_request.clone();
+    if file_request.len() > 0 {
+      file_request_name = &file_request[1..file_request.len()];
+    }    
+    file_request = file_request_name;
+    println!("file_request: {:?}",file_request);
+    ("HTTP/1.1 200 OK", file_request)
+    // ("HTTP/1.1 200 OK" , "hello.html")
   } else {
+    println!("reqeust: {}", request_line);
     ("HTTP/1.1 404 NOT FOUND", "404.html")
   };
 
   let contents = fs::read_to_string(filename).unwrap();
+  println!("{contents}");
   let length = contents.len();
 
   let response = format!(
@@ -38,4 +59,12 @@ fn handle_connection(mut stream: TcpStream) {
   );
 
   stream.write_all(response.as_bytes()).unwrap();
+
+  // let buf2 = BufReader::new(&mut stream);
+  // let http_request: Vec<_> = buf2
+  //   .lines()
+  //   .map(|result| result.unwrap())
+  //   .take_while(|line| !line.is_empty())
+  //   .collect();
+  // println!("Request: {:#?}", http_request);
 }
